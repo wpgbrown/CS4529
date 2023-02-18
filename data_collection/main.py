598e23cad8d97d0d1f4bb71c42f0a2b85432305c
William Brown <william@wpgbrown.co.uk>
2023-02-18 17:19:28 +0000

    Changes

--- data_collection/main.py
+++ data_collection/main.py
@@ -14,3 +14,6 @@ elasticsearch_request_headers = {'kbn-xsrf': 'true', 'content-type': 'applicatio
 gerrit_search_url = 'https://wikimedia.biterg.io/data/gerrit/_search'
 git_search_url = 'https://wikimedia.biterg.io/data/git/_search'
 phabricator_search_url = 'https://wikimedia.biterg.io/data/phabricator/_search'
+
+extensions_list = [ line.strip() for line in open("extensions_list.txt", "r").readlines() ]
+
