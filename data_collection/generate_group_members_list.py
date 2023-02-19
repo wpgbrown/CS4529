dcea420bbb99101399a52e7e8bf2cc5ae6a7d9a7
William Brown <william@wpgbrown.co.uk>
2023-02-19 22:35:32 +0000

    Changes

--- data_collection/generate_group_members_list.py
+++ data_collection/generate_group_members_list.py
@@ -1,18 +1,29 @@
+import json
 import pickle
 import string
-import itertools
+import time
+
+import more_itertools
 import requests
 
-from . import common
+from data_collection import common
+
 
 def generate_reviewers_list( repositories ):
-    members_of_groups = []
+    members_of_groups = {}
     try:
-        for repository_batch in itertools(repositories):
-            # Batch repos by 100s
-            members_of_groups.append(get_reviewer_data_for_gerrit_repo(repository))
+        for batch_id, repository_batch in enumerate(more_itertools.chunked(repositories, 25)):
+            print("Batch", batch_id)
+            # Batch repos by 25 for request
+            request_url = common.gerrit_api_url_prefix + "access/?project=" + repository_batch[0]
+            for repository in repository_batch[1:]:
+                request_url += "&project=" + repository
+            members_of_groups.update(json.loads(requests.get(request_url, auth=common.secrets.gerrit_http_credentials()).text.replace(")]}'", '')))
+            time.sleep(1)
     finally:
-        pickle.dump( members_of_groups, open( "raw_data/members_of_groups_data.dump", "w" ) )
+        json.dump( members_of_groups, open( "raw_data/members_of_groups_data.json", "w" ) )
 
-def get_reviewer_data_for_gerrit_repo( repository: string ):
-    return requests.get( common.gerrit_api_url_prefix + "access/?project=" + repository + "/members" )
+def get_reviewer_data_for_extensions():
+    generate_reviewers_list([ "mediawiki/extensions/" + extension for extension in common.extensions_list ])
+
+get_reviewer_data_for_extensions()
