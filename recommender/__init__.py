0452b039de3325e9e252bf3ca0a9eade258f3b31
William Brown <william@wpgbrown.co.uk>
2023-04-13 17:31:21 +0100

    Min viable product for simple recommender in

--- recommender/__init__.py
+++ recommender/__init__.py
@@ -1,11 +1,38 @@
+import json
 import weakref
+from functools import lru_cache
 from weakref import ReferenceType
 from collections.abc import Iterable, Sized
 import logging
-from typing import List, Union, Optional, Iterator
+from typing import List, Union, Optional, Iterator, Any
+import itertools
+
+import common
+
+
+class WeightingsBase:
+    ALL_TIME = 'all time'
+    LAST_YEAR = 'last year'
+    LAST_3_MONTHS = 'last three months'
+    LAST_MONTH = 'last month'
+    def __init__(self, weightings_file):
+        weightings = json.load(open(weightings_file, 'r'))
+        weightings: dict
+        self._weightings = weightings
+
+
+@lru_cache(maxsize=1)
+def load_members_of_mediawiki_repos() -> dict:
+    members_list = common.path_relative_to_root('data_collection/raw_data/members_of_mediawiki_repos.json')
+    return json.load(open(members_list, 'r'))
+
+def get_members_of_repo(repository: str) -> List[dict[str, Any]]:
+    groups_with_rights_to_merge = load_members_of_mediawiki_repos()['groups_for_repository'][repository].keys()
+    groups_with_rights_to_merge = list(filter(lambda x: x not in common.group_exclude_list, groups_with_rights_to_merge))
+    return list(itertools.chain.from_iterable([members for group_id, members in load_members_of_mediawiki_repos()['members_in_group'].items() if group_id in groups_with_rights_to_merge]))
 
 class RecommendedReviewer:
-    def __init__(self, email: Optional[str] = None, names: Union[str, List[str]] = None, score: float = 0, parent: Optional[ReferenceType] = None):
+    def __init__(self, email: Optional[str] = None, names: Union[str, 'Names', List[str]] = None, score: float = 0, parent: Optional[ReferenceType] = None, has_rights_to_merge: Optional[bool] = None):
         """
         Creates a recommendation for a given reviewer that has a given score to rank this
         recommendation against other recommendations.
@@ -17,15 +44,20 @@ class RecommendedReviewer:
         :param score: The score of the recommendation. Optional, can be changed later
         :param parent: A weak reference to the Recommendations list this is stored in.
         """
+        self._email = email
+        if self._email is not None:
+            self._email = self._email.lower()
         if names is None:
             names = []
         if isinstance(names, str):
             names = [names]
         if not len(names) and not email:
             raise ValueError("An email or a name must be provided.")
-        names = Names(names, weakref.ref(self))
-        self._email = email
+        if not isinstance(names, Names):
+            names = Names(names, weakref.ref(self))
         self._names = names
+        self.has_rights_to_merge = has_rights_to_merge
+        """Whether this user has the rights to merge the change."""
         self.score = score
         """The score associated with the recommendation. Larger the better."""
         self.parent_weak_ref = parent
@@ -43,16 +75,28 @@ class RecommendedReviewer:
 
     @email.setter
     def email(self, email: str):
+        logging.debug("Setting email " + str(email))
         if self._email:
             logging.warning("Attempt to overwrite already set email. This was ignored.")
             return
-        self._email = email
+        self._email = email.lower()
         if self.parent_weak_ref is not None:
             parent = self.parent_weak_ref()
             parent: Recommendations
             if parent is not None:
                 # Indicate to the parent recommendations list that an email was specified.
                 parent._email_specified_for_reviewer(self)  # noqa
+
+    def add_score(self, value: float, *weightings: float) -> None:
+        """
+        Adds to the score attribute with a defined weighting that modifies the value
+
+        :param value: The raw value
+        :param weightings: The weighting(s) to apply to it (which are multiplied to the value)
+        """
+        for weighting in weightings:
+            value *= weighting
+        self.score += value
 
     def __lt__(self, other):
         if not isinstance(other, RecommendedReviewer):
@@ -65,9 +109,12 @@ class RecommendedReviewer:
         return self.score > other.score
 
     def __str__(self):
-        return_string = "Recommending %s" % self.email
+        return_string = "Recommending"
+        if len(self.email or ''):
+            return_string += " " + self.email
+            if len(self.names):
+                return_string += " known by username"
         if len(self.names):
-            return_string += " known by username"
             if len(self.names) == 1:
                 return_string += " " + self.names[0]
             elif len(self.names) == 2:
@@ -82,9 +129,12 @@ class Names(Iterable, Sized):
         self.parent_weak_ref = parent_weak_ref
 
     def add(self, name: str):
+        name = name
+        if name in self._names:
+            return
         self._names.append(name)
         parent = self.parent_weak_ref()
-        if parent is None:
+        if parent is None or not parent.email:
             return
         parent: RecommendedReviewer
         if parent.parent_weak_ref is None:
@@ -97,10 +147,13 @@ class Names(Iterable, Sized):
         # _update_index is intended for use only by this class, so the underscore is added
         #  to indicate this. Users attempting to add a name for an email using this method
         #  won't work, so this underscore added to hide the method from suggestions in IDEs.
-        grandparent._update_index(name, parent.email) # noqa
+        grandparent._update_index(name, parent.email, True, parent) # noqa
 
     def __iter__(self) -> Iterator[str]:
         return iter(self._names)
+
+    def __contains__(self, item: str):
+        return item in self._names
 
     def __len__(self) -> int:
         return len(self._names)
@@ -110,6 +163,7 @@ class Names(Iterable, Sized):
 
     def __getitem__(self, index):
         return self._names[index]
+
 class Recommendations:
     def __init__(self):
         self._recommendations_by_email = {}
@@ -123,22 +177,26 @@ class Recommendations:
     def recommendations(self):
         return dict(self._recommendations_by_email, **self._recommendations_by_name).values()
 
-    def ordered_by_score(self) -> List[RecommendedReviewer]:
+    def ordered_by_score(self, filter_for_users_that_can_merge: bool = False) -> List[RecommendedReviewer]:
         """
         Returns the recommendations ordered by their score.
 
+        :param filter_for_users_that_can_merge: TODO: Better parameter name
         :return: The ordered recommendations
         """
+        if filter_for_users_that_can_merge:
+            return sorted(filter(lambda x: x.has_rights_to_merge, self.recommendations), reverse=True)
         return sorted(self.recommendations, reverse=True)
 
-    def top_n(self, n: int) -> List[RecommendedReviewer]:
+    def top_n(self, n: int, filter_for_users_that_can_merge: bool = False) -> List[RecommendedReviewer]:
         """
         Gets the top N recommendations.
 
+        :param filter_for_users_that_can_merge: TODO: Better parameter name
         :param n: The number of recommendations to return
         :return: The top N recommendations
         """
-        return self.ordered_by_score()[:n]
+        return self.ordered_by_score(filter_for_users_that_can_merge)[:n]
 
     def add(self, recommendation: RecommendedReviewer) -> 'Recommendations':
         """
@@ -176,7 +234,7 @@ class Recommendations:
         recommendation.parent_weak_ref = weakref.ref(self)
         return self
 
-    def _update_index(self, name: str, email: str) -> 'Recommendations':
+    def _update_index(self, name: str, email: str, merge_entries_with_same_name: bool = False, recommended_reviewer: Optional[RecommendedReviewer] = None) -> 'Recommendations':
         """
         Updates the index of names to email addresses.
 
@@ -190,6 +248,14 @@ class Recommendations:
         """
         if name in self._names_to_emails.keys():
             logging.info("Name already defined to an email, but updating the index.")
+            """if merge_entries_with_same_name and recommended_reviewer is not None and self.get_reviewer_by_email(self._names_to_emails[name]) is not recommended_reviewer:
+                if name == "umherirrender":
+                    print("AHAHQAHA")
+                    print(name)
+                    print(self.get_reviewer_by_email(self._names_to_emails[name]).names)
+                    print(recommended_reviewer.email)
+                    print(self.get_reviewer_by_email(self._names_to_emails[name]).email)
+                self._merge_reviewer_entries(self.get_reviewer_by_email(self._names_to_emails[name]), recommended_reviewer)"""
         self._names_to_emails[name] = email
         return self
 
@@ -220,6 +286,7 @@ class Recommendations:
         :param remove_other_entry: Removes the second entry from the list if True. Set to False if it doesn't exist.
         :return:
         """
+        logging.debug("Merge! " + str(base.email or 'No email'))
         # Merge the second entries attributes into the first entry
         for name in other_entry.names:
             base.names.add(name)
@@ -234,7 +301,7 @@ class Recommendations:
                     # Remove the second entry from the recommendations list
                     del self._recommendations_by_email[other_entry.email]
             for name in other_entry.names:
-                if other_entry is self._recommendations_by_name[name]:
+                if name in self._recommendations_by_name and other_entry is self._recommendations_by_name[name]:
                     del self._recommendations_by_name[name]
 
     def get_reviewer_by_email(self, email: str) -> Union[RecommendedReviewer, None]:
@@ -260,6 +327,31 @@ class Recommendations:
             # Then check if the name is associated to a reviewer without an email
             return self._recommendations_by_name[name]
         return None
+
+        """
+
+        :param email:
+        :return:
+        """
+        logging.debug("Getting by email")
+        reviewer = self.get_reviewer_by_email(email)
+        if reviewer is None:
+            reviewer = RecommendedReviewer(email)
+            self.add(reviewer)
+        return reviewer
+
+    def get_reviewer_by_name_or_create_new(self, name: str) -> RecommendedReviewer:
+        """
+
+        :param name:
+        :return:
+        """
+        logging.debug("Getting by name")
+        reviewer = self.get_reviewer_by_name(name)
+        if reviewer is None:
+            reviewer = RecommendedReviewer(names=name)
+            self.add(reviewer)
+        return reviewer
 
     def __getitem__(self, item):
         reviewer_by_email = self.get_reviewer_by_email(item)
--- recommender/simple_recommender.py
+++ recommender/simple_recommender.py
@@ -10,23 +10,50 @@ from functools import lru_cache
 from requests import HTTPError
 from data_collection import git_blame
 from data_collection.preprocessing import reviewer_votes_to_percentages, comment_counts_to_percentages
-from recommender import Recommendations, RecommendedReviewer
+from recommender import Recommendations, RecommendedReviewer, WeightingsBase, get_members_of_repo
 
 # Add parent directory to the path incase it's not already there
 sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import common
 
+#DEBUG
+class SimpleWeightings(WeightingsBase):
+    def __init__(self):
+        super().__init__(common.path_relative_to_root("recommender/simple_recommender_weightings.json"))
+        # -- Parse the weightings JSON file --
+        # Line counts
+        self.lines_count = {
+            'authors': {},
+            'committers': {}
+        }
+        if 'git blame' in self._weightings:
+            if 'authors' in self._weightings['git blame'] and 'lines count' in self._weightings['git blame']['authors']:
+                self.lines_count['authors'] = self._weightings['git blame']['authors']['lines count']
+            if 'committers' in self._weightings['git blame'] and 'lines count' in self._weightings['git blame']['committers']:
+                self.lines_count['committers'] = self._weightings['git blame']['committers']['lines count']
+        # Votes
+        self.votes = {}
+        if 'votes' in self._weightings:
+            self.votes = self._weightings['votes']
+        # Comments
+        self.comments = {}
+        if 'comments' in self._weightings:
+            self.comments = self._weightings['comments']
+
+
+weightings = SimpleWeightings()
+
 if __name__ == "__main__":
     logging.basicConfig(filename=common.path_relative_to_root("logs/simple_recommender.log.txt"), level=logging.DEBUG)
 
-@lru_cache()
+@lru_cache(maxsize=1)
 def get_reviewer_data():
     percentage_list = common.path_relative_to_root('data_collection/raw_data/reviewer_vote_percentages_for_repos.json')
     if not os.path.exists(percentage_list):
         comment_counts_to_percentages.convert_data_to_percentages()
     return json.load(open(percentage_list, 'r'))
 
-@lru_cache()
+@lru_cache(maxsize=1)
 def get_comment_data():
     percentage_list = common.path_relative_to_root('data_collection/raw_data/comment_count_percentages_by_author_for_repo.json')
     if not os.path.exists(percentage_list):
@@ -64,16 +91,20 @@ def recommend_reviewers_for_patch(change_id: str, repository: str = '', branch: 
     #  even if they have been provided by the caller.
     repository = change_info['project']
     branch = change_info['branch']
+    # Initialise the recommendations list
+    recommendations = Recommendations()
     # Get the files modified (added, changed or deleted) by the change
     latest_revision_sha = list(change_info['revisions'].keys())[0]
-    git_blame_stats_for_files_in_head = {}
+    information_about_change_including_git_blame_stats_in_head = {}
     for filename, info in change_info['revisions'][latest_revision_sha]['files'].items():
         # Add deleted and changed files to files modified from the base
         info: dict
         if 'status' not in info.keys():
             # File just modified (not created, moved or deleted), so other authors can likely help
-            git_blame_stats_for_files_in_head[filename] = \
-                git_blame.git_blame_stats_for_head_of_branch(filename, repository, branch)
+            information_about_change_including_git_blame_stats_in_head[filename] = info
+            information_about_change_including_git_blame_stats_in_head[filename].update(
+                {'blame_stats': git_blame.git_blame_stats_for_head_of_branch(filename, repository, branch)}
+            )
         else:
             match info['status']:
                 case 'D' | 'W':
@@ -82,7 +113,8 @@ def recommend_reviewers_for_patch(change_id: str, repository: str = '', branch: 
                     #  already present will be kept if this is merged, said
                     #  authors and committers are likely to provide some
                     #  useful thoughts on this.
-                    git_blame_stats_for_files_in_head[filename] = info.update(
+                    information_about_change_including_git_blame_stats_in_head[filename] = info
+                    information_about_change_including_git_blame_stats_in_head[filename].update(
                         {'blame_stats': git_blame.git_blame_stats_for_head_of_branch(filename, repository, branch)}
                     )
                 case 'R' | 'C':
@@ -90,22 +122,95 @@ def recommend_reviewers_for_patch(change_id: str, repository: str = '', branch: 
                     #  or the file that was copied likely have understanding about whether a rename
                     #  or copy would make sense here.
                     # TODO: Test with a change that has a copied file.
-                    git_blame_stats_for_files_in_head[filename] = info.update(
+                    information_about_change_including_git_blame_stats_in_head[filename] = info
+                    information_about_change_including_git_blame_stats_in_head[filename].update(
                         {'blame_stats': git_blame.git_blame_stats_for_head_of_branch(info['old_path'], repository, branch)}
                     )
-    logging.debug("Git blame files from base: " + str(git_blame_stats_for_files_in_head))
+    logging.debug("Git blame files from base: " + str(information_about_change_including_git_blame_stats_in_head))
+    total_delta_over_all_files = sum([info['size_delta'] for info in information_about_change_including_git_blame_stats_in_head.values()])
+    for file, info in information_about_change_including_git_blame_stats_in_head.items():
+        logging.debug(info)
+        for author_email, author_info in info['blame_stats']['authors'].items():
+            reviewer = recommendations.get_reviewer_by_email_or_create_new(author_email)
+            for name in author_info['name']:
+                reviewer.names.add(name)
+            for key, weighting in weightings.lines_count['authors'].items():
+                reviewer.add_score(author_info[key.replace(' ', '_') + '_lines_count'], info['size_delta'] / total_delta_over_all_files, weighting)
+        for committer_email, committer_info in info['blame_stats']['committers'].items():
+            reviewer = recommendations.get_reviewer_by_email_or_create_new(committer_email)
+            for name in committer_info['name']:
+                reviewer.names.add(name)
+            for key, weighting in weightings.lines_count['committers'].items():
+                reviewer.add_score(committer_info[key.replace(' ', '_') + '_lines_count'], info['size_delta'] / total_delta_over_all_files, weighting)
+    # TODO: Use users with +2 to match usernames to emails. Current system doesn't handle variations in usernames well.
+    del information_about_change_including_git_blame_stats_in_head
     # Get previous reviewers for changes
-    reviewer_votes_for_repo = get_reviewer_data()[repository]
-    logging.debug("Reviewer votes: " + str(reviewer_votes_for_repo))
+    reviewer_votes_for_current_repo = get_reviewer_data()[repository]
+    logging.debug("Reviewer votes: " + str(reviewer_votes_for_current_repo))
+    for vote_weighting_key, vote_weightings in weightings.votes.items():
+        # Translate key into format used by the script
+        # TODO: Remove this by updating the definitions in the script that makes this?
+        match vote_weighting_key:
+            case 'Total votes':
+                vote_weighting_key = 'Gerrit approval actions count'
+            case '+1 code review votes':
+                vote_weighting_key = "1 code review votes"
+            case '+2 code review votes':
+                vote_weighting_key = "2 code review votes"
+        for key, weighting in vote_weightings.items():
+            # TODO: Remove this by updating the definitions in the script that makes this
+            match key:
+                case weightings.LAST_MONTH:
+                    key = "last 30 days"
+                case weightings.LAST_3_MONTHS:
+                    key = "last 3 months"
+                case weightings.ALL_TIME:
+                    key = "all"
+            for reviewer_name, comment_percentage in reviewer_votes_for_current_repo[key].items():
+                reviewer = recommendations.get_reviewer_by_name_or_create_new(reviewer_name)
+                reviewer.add_score(comment_percentage[vote_weighting_key], weighting)
+    del reviewer_votes_for_current_repo
     # Get authors of previous comments
-    comments_for_repo = get_comment_data()[repository]
-    logging.debug("Comments: " + str(comments_for_repo))
-    recommended_reviewers = Recommendations()
-    for file, info in git_blame_stats_for_files_in_head.items():
-        for author_email, author_info in info['authors'].items():
-            reviewer = RecommendedReviewer(author_email)
+    comments_for_current_repo = get_comment_data()[repository]
+    logging.debug("Comments: " + str(comments_for_current_repo))
+    for key, weighting in weightings.comments.items():
+        # TODO: Remove this by updating the definitions in the script that makes this
+        match key:
+            case weightings.LAST_MONTH:
+                key = "last 30 days"
+            case weightings.LAST_3_MONTHS:
+                key = "last 3 months"
+            case weightings.ALL_TIME:
+                key = "all"
+        for reviewer_name, comment_percentage in comments_for_current_repo[key].items():
+            reviewer = recommendations.get_reviewer_by_name_or_create_new(reviewer_name)
+            reviewer.add_score(comment_percentage, weighting)
+    del comments_for_current_repo
     # TODO: Filter list for users with +2?
-    return recommended_reviewers
+    # Initialise list based on who has rights to merge for the repository
+    users_with_rights_to_merge = get_members_of_repo(repository)
+    logging.debug("users with right to merge: " + str(users_with_rights_to_merge))
+    for user in users_with_rights_to_merge:
+        if 'email' in user and user['email']:
+            reviewer = recommendations.get_reviewer_by_email_or_create_new(user['email'])
+        elif 'name' in user and user['name']:
+            reviewer = recommendations.get_reviewer_by_name_or_create_new(user['name'])
+        elif 'username' in user and user['username']:
+            reviewer = recommendations.get_reviewer_by_name_or_create_new(user['username'])
+        elif 'display_name' in user and user['display_name']:
+            reviewer = recommendations.get_reviewer_by_name_or_create_new(user['display_name'])
+        else:
+            continue
+        if 'name' in user and user['name']:
+            reviewer.names.add(user['name'])
+        if 'username' in user and user['username']:
+            reviewer.names.add(user['username'])
+        if 'display_name' in user and user['display_name']:
+            reviewer.names.add(user['display_name'])
+        reviewer.has_rights_to_merge = True
+    for reviewer in filter(lambda x: x.has_rights_to_merge is not True, recommendations.recommendations):
+        reviewer.has_rights_to_merge = False
+    return recommendations
 
 if __name__ == '__main__':
     argument_parser = argparse.ArgumentParser(description="A simple implementation of a tool that recommends reviewers for the MediaWiki project")
@@ -157,5 +262,8 @@ if __name__ == '__main__':
     for change in change_ids_with_repo_and_branch:
         try:
             recommended_reviewers = recommend_reviewers_for_patch(change['change_id'], change["repository"], change["branch"])
+            logging.debug("Recommendations: " + str(recommended_reviewers))
+            for recommendation in recommended_reviewers.ordered_by_score():
+                print(recommendation)
         except HTTPError as e:
             print("Recommendations for change", change["change_id"], "failed with HTTP status code", str(e.response.status_code) + ". Check that this is correct and try again later.")
